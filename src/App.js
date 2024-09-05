b6e31edc74d6e112173ec14510e031656d2f3808
eshumphries <eshumphries@outlook.com>
2024-09-04 22:05:01 -0400

    import Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Switch, Link} from "react-router-dom";
 import About from './About';
 import Home from './Home';
 
